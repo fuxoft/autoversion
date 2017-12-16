51082dcce932d6fe1dbe8994936a3f8242bfb460
Frantisek Fuka <fuka@fuxoft.cz>
2017-12-16 02:16:37 +0100

    Init

---
+++ autoversion.lua
@@ -0,0 +1,55 @@
+#!/usr/bin/env luajit
+-- Autoversion
+-- This is used to automatically update the version of your Lua script.
+-- It looks for a following string in your script file and updates it accordingly:
+
+-- [[[[*<= Version '20171215g' =>*]]]]
+
+-- Note that the initial version has to be exactly 9 characters, 8 numbers and a lowercase letter
+-- It updates ONLY the first occurence of this very special string in the file.
+
+local function main()
+	local fname = arg[1]
+	assert(fname, "No file name supplied.")
+	local fd = io.open(fname)
+	assert (fd, "Cannot open file: "..fname)
+	local txt = assert(fd:read("*a"))
+	assert(fd:close())
+	print("Read file "..fname..", "..(#txt).." bytes.")
+	local found = false
+	local newversion, oldversion
+	txt = txt:gsub("%[%[%[%[%*<= Version '(.........)' =>%*%]%]%]%]", function(str)
+		found = true
+		oldversion = str
+		local date, letter = str:match '(%d%d%d%d%d%d%d%d)(%l)'
+		assert(date and letter, "Invalid version format.")
+		local newdate = os.date("%Y%m%d")
+		if date > newdate then
+			error(string.format("New date (%s) is smaller than the original date (%s)", newdate, date))
+		end
+		local newletter = "a"
+		if date == newdate then
+			newletter = string.char(letter:byte() + 1)
+			if newletter > "z" then
+				error("Original version letter is 'z', cannot increase it.")
+			end
+		end
+		newversion = newdate .. newletter
+		print("New version", newversion)
+		return "[[[[*<= Version '"..newversion.."' =>*]]]]"
+	end, 1)
+	if not found then
+		error("Didn't find the magic version string in the file.")
+	end
+	--local tmpfn = "/tmp/fuxoft_autoversion"..os.time()..".txt"
+	local fd = assert(io.open(fname, "w"))
+	print("The updated file follows:")
+	print("-------------------------------------------")
+	print(txt)
+	assert(fd:write(txt))
+	assert(fd:close())
+	print("------------------------------------------- OK")
+	print("Succesfully updated file ".. fname .. " from version " .. oldversion .. " to "..newversion)
+end
+
+main()
